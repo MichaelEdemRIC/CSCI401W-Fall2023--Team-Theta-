918cec29fcfd40265abeb9a7b337e95cddcf9efa
sp-64 <sparks_1017@email.ric.edu>
2023-11-11 13:23:11 -0500

    update
    update

--- src/frontend/src/pages/ProductPage.tsx
+++ src/frontend/src/pages/ProductPage.tsx
@@ -1,17 +1,5 @@
 // ProductPage.tsx
-import { useParams } from 'react-router-dom';
-import { Product } from '../components/Product';
-
-
-
 export function ProductPage() {
-  const { productId } = useParams();
 
-  return (
-    <div>
-      <h1>{productId}</h1>
-      {/* Render details of the selected product */}
-      {/* You can also pass selectedProduct to other components as needed */}
-    </div>
-  );
+  return <h1>Product</h1>
 }
