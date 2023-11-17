9d39649a01a299ac77abe806bd2deb3e5c1d73e4
astanley8282 <143468809+astanley8282@users.noreply.github.com>
2023-11-16 20:37:59 -0500

    remove student from urls

--- src/backend/django_react_proj/django_react_proj/urls.py
+++ src/backend/django_react_proj/django_react_proj/urls.py
@@ -5,8 +5,6 @@ from prices import views as price_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # re_path(r'^api/students/$', student_views.students_list),
-    # re_path(r'^api/students/([0-9])$', student_views.students_detail),
     path('api/get_data/', price_views.get_data),
     path('api/get_data_id/<int:pk>/', price_views.get_data_id),
     path('api/add_item/', price_views.add_item),
