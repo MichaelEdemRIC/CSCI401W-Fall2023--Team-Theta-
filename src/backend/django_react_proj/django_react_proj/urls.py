a59fff5a74c27ef51d8c8500c782075f67bfb59b
astanley8282 <143468809+astanley8282@users.noreply.github.com>
2023-11-16 19:28:12 -0500

    remove students app (tutorial)

--- src/backend/django_react_proj/django_react_proj/urls.py
+++ src/backend/django_react_proj/django_react_proj/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from students import views as student_views
+# from students import views as student_views
 from prices import views as price_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^api/students/$', student_views.students_list),
-    re_path(r'^api/students/([0-9])$', student_views.students_detail),
+    # re_path(r'^api/students/$', student_views.students_list),
+    # re_path(r'^api/students/([0-9])$', student_views.students_detail),
     path('api/get_data/', price_views.get_data),
     path('api/get_data_id/<int:pk>/', price_views.get_data_id),
     path('api/add_item/', price_views.add_item),
