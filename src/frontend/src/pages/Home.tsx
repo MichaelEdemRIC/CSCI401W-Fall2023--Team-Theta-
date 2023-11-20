ba0cf6f20a6f547032b86ae2c240c5ffa15af8d4
sp-64 <sparks_1017@email.ric.edu>
2023-11-20 01:24:33 -0500

    product page and search
    connected to api and created the product detail view pages.

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,18 +1,43 @@
-import storeItems from "../data/products.json";
 import { Col, Row } from "react-bootstrap";
 import { ItemCard } from "../components/ItemCard";
+import { useState, useEffect } from "react";
+import { Product } from "../components/Product";
+import axios from "axios";
 
 export function Home() {
+  const [items, setItems] = useState<Product[] | null>(null);
+  const itemsEndpoint = "http://localhost:8000/api/get_data/";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(itemsEndpoint);
+        setItems(response.data);
+      } catch (error) {
+        console.error(error);
+        setItems([]); // Set an empty array or handle the error state accordingly
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this runs only once after mount
+
+  if (items === null) {
+    // Loading state, you can customize this to show a loading spinner or message
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>Home</h1>
       <Row md={3} xs={2} lg={4} className="g-3">
-        {storeItems.map((storeitem) => (
-          <Col key={storeitem.id}>
-            <ItemCard item={storeitem} />
+        {items.map((item) => (
+          <Col key={item.pk}>
+            <ItemCard item={item} />
           </Col>
         ))}
       </Row>
     </>
   );
 }
+
--- src/frontend/src/pages/SearchResults.tsx
+++ src/frontend/src/pages/SearchResults.tsx
@@ -1,18 +1,68 @@
-import storeItems from "../data/searchresults.json";
 import { Col, Row } from "react-bootstrap";
 import { ItemCard } from "../components/ItemCard";
+import { Product } from "../components/Product";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 export function SearchResults() {
-  return (
-    <>
-      <h1>Results</h1>
-      <Row md={3} xs={2} lg={4} className="g-3">
-        {storeItems.map((item) => (
-          <Col key={item.id}>
-            <ItemCard item={item}/>
+  const location = useLocation();
+  const query:string = location.state;
+  
+  const [searchData, setSearchData] = useState<Product | Product[]>([]);
+  const searchEndpoint = "http://localhost:8000/api/search_name/";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(searchEndpoint + query);
+        console.log(response.data);
+        setSearchData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  if (searchData === null) {
+    // Loading state, you can customize this to show a loading spinner or message
+    return <p>Loading...</p>;
+  }
+
+  if (Array.isArray(searchData)) {
+    // It's an array, you can map over it
+    return (
+      <>
+        <h1>Search Results</h1>
+        <Row md={3} xs={2} lg={4} className="g-3">
+          {searchData.map((product: Product) => (
+            <Col key={product.pk}>
+              <ItemCard item={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
+  } else if (searchData) {
+    // It's a single item, not an array
+    return (
+      <>
+        <h1>Search Result</h1>
+        <Row md={3} xs={2} lg={4} className="g-3">
+          <Col key={searchData.pk}>
+            <ItemCard item={searchData} />
           </Col>
-        ))}
-      </Row>
-    </>
-  );
+        </Row>
+      </>
+    );
+  } else {
+    // No results
+    return (
+      <>
+        <h1>No Results Found</h1>
+      </>
+    );
+  }
 }
