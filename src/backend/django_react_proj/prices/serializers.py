c43dde9970a7a05feb15f3ea02553c1fcfa53c72
astanley8282 <143468809+astanley8282@users.noreply.github.com>
2023-11-28 23:24:05 -0500

    change product serializer to return id instead

--- src/backend/django_react_proj/prices/serializers.py
+++ src/backend/django_react_proj/prices/serializers.py
@@ -6,9 +6,11 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product 
-        fields = ('pk', 'name', 'amzMSRP', 'amzCurrentPrice', 'walMSRP', 'walCurrentPrice', 'lowestPrice', 'img', 'amzURL', 'walURL', 'dateAdded')
+        fields = ('id', 'name', 'amzMSRP', 'amzCurrentPrice', 'walMSRP', 'walCurrentPrice', 'lowestPrice', 'img', 'amzURL', 'walURL', 'dateAdded')
 
 class WishlistSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
     class Meta:
         model = Wishlist
         fields = ('id', 'user', 'product', 'dateAdded')
