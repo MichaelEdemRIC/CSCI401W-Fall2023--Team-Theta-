b545ef4d1c30b9c5dfe008340dbb690392a52aef
sp-64 <sparks_1017@email.ric.edu>
2023-12-04 00:32:33 -0500

    frontend can login and sign up from backend

--- src/frontend/src/pages/RegisterPage.tsx
+++ src/frontend/src/pages/RegisterPage.tsx
@@ -1,26 +1,68 @@
 
-
-
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
-import "../LoginApp.css"
-
+import '../LoginApp.css'
+import { ChangeEvent, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function RegisterPage() {
+    const registerURL = "http://localhost:8000/signup/";
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [token, setToken] = useState<string>('');
+    const navigate = useNavigate();
 
+    const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+      };
+    
+      const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+      };
+
+      const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Prevent the default form submission\
+        
+        // Log the data before sending the request
+        console.log("Data to be sent:", { username, password });
+
+        try {
+            const response = await axios.post(registerURL, { username, password });
+            console.log("Response:", response.data);
+            const token = response.data.token;
+            setToken(token)
+            navigate('/');
+        } catch (error:any) {
+            console.error("Error:", error.message);
+            // Handle errors, such as displaying an error message to the user
+        }
+    };
     return (
         <div className="text-center m-5-auto">
             <h2>Join us for great deals</h2>
             <h5>Create an account</h5>
-            <form action="/home" className="login">
+            <form action="/home" className="login" onSubmit={handleSubmit}>
                 <p>
                     <label>create a Username</label><br/>
-                    <input type="text" name="first_name" required />
+                    <input 
+                       onChange={handleUsernameChange} 
+                       type="text" 
+                       name="username" 
+                       value={username}
+                       required 
+                    />
                 </p>
                 
                 <p>
                     <label>create a Password</label><br/>
-                    <input type="password" name="password" required/>
+                    <input 
+                        onChange={handlePasswordChange} 
+                        type="password" 
+                        name="password" 
+                        value={password}
+                        required 
+                    />
                 </p>
                 
                 <p>
